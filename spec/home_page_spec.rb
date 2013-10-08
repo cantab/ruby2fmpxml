22fb7fd20caf49b31e5de465c7225c3492a93507
Chong-Yee Khoo <mail@cykhoo.com>
2013-10-08 14:30:35 +0800

    add more tests for the home page

--- spec/home_page_spec.rb
+++ spec/home_page_spec.rb
@@ -14,9 +14,28 @@ describe 'ruby2fmpxml web app', type: :feature do
       expect(home_page).to include('Ruby to FileMaker Pro XML')
     end
 
-    it "a form with textarea" do
-      expect(home_page).to include('textarea id="my_string"')
+    it "has a form with textarea" do
       expect(home_page).to have_selector('textarea#my_string')
+    end
+
+    it "has a submit button" do
+      expect(home_page).to have_selector('input#button_submit')
+    end
+
+    describe "form submission" do
+      before do
+        visit '/'
+        page.fill_in 'my_string', with: "It's a sledge"
+        click_button 'Submit'
+      end
+
+      it "submits the form" do
+        expect(response).to be_ok
+      end
+
+      it "returns the right content" do
+        expect(page.body).to have_content("Vg'f n fyrqtr")
+      end
     end
   end
 end
