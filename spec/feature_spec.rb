ea50559df945e93d59627ddf093df937fd8e7225
Chong-Yee Khoo <mail@cykhoo.com>
2013-10-08 13:51:11 +0800

    add more examples for home page

--- spec/feature_spec.rb
+++ spec/feature_spec.rb
@@ -2,9 +2,20 @@ require 'spec_helper'
 
 describe 'ruby2fmpxml web app' do
 
-  it "has a home page" do
-    get '/'
-    expect(last_response).to be_ok
-    expect(last_response.body).to include('Ruby to FileMaker Pro XML')
+  describe "home page" do
+    let(:response) { get '/' }
+    let(:home_page) { response.body }
+
+    it "returns a page" do
+      expect(response).to be_ok
+    end
+
+    it "has the right header" do
+      expect(home_page).to include('Ruby to FileMaker Pro XML')
+    end
+
+    it "a form with textarea" do
+      expect(home_page).to include('textarea id="my_string"')
+    end
   end
 end
