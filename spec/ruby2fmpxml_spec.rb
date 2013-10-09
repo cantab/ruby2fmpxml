0a20745a5cf45c1df0c398956432cab9439117ac
Chong-Yee Khoo <mail@cykhoo.com>
2013-10-09 17:18:14 +0800

    improve the spec for ruby2fmpxml functions

--- spec/ruby2fmpxml_spec.rb
+++ spec/ruby2fmpxml_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe 'ruby2fmpxml web app', type: :feature do
+describe 'ruby2fmpxml', type: :feature do
 
   describe "home page" do
     let(:response) { get '/' }
@@ -18,15 +18,49 @@ describe 'ruby2fmpxml web app', type: :feature do
       expect(home_page).to have_selector('input#button_submit')
     end
 
-    describe "form submission" do
+    describe "form submission with POST" do
       before do
         visit '/'
         page.fill_in 'my_string', with: "It's a sledge"
         click_button 'Submit'
       end
 
-      it "returns the right content" do
-        expect(page.body).to have_content("Vg'f n fyrqtr")
+      it "returns an XML document" do
+        expect(page.body).to include("<?xml")
+      end
+
+      it "XML document is in the right grammar" do
+        expect(page.body).to include('FMPXMLRESULT xmlns="http://www.filemaker.com/fmpxmlresult"')
+      end
+
+      it "has the input string" do
+        expect(page.body).to include("<COL>\n        <DATA>It's a sledge</DATA>\n      </COL>")
+      end
+
+      it "has the output Rot13 result" do
+        expect(page.body).to include("<COL>\n        <DATA>Vg'f n fyrqtr</DATA>\n      </COL>")
+      end
+    end
+
+    describe "direct access with GET" do
+      before do
+        visit '/rot13/It%27s%20a%20sledge'
+      end
+
+      it "returns an XML document" do
+        expect(page.body).to include("<?xml")
+      end
+
+      it "XML document is in the right grammar" do
+        expect(page.body).to include('FMPXMLRESULT xmlns="http://www.filemaker.com/fmpxmlresult"')
+      end
+
+      it "has the input string" do
+        expect(page.body).to include("<COL>\n        <DATA>It's a sledge</DATA>\n      </COL>")
+      end
+
+      it "has the output Rot13 result" do
+        expect(page.body).to include("<COL>\n        <DATA>Vg'f n fyrqtr</DATA>\n      </COL>")
       end
     end
   end
